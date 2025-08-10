aa1a3a5415842903dd456e53488455e92457e83c
BCEESAY10 <bamfaceesay30@gmail.com>
2025-08-10 03:21:02 +0000

    import images and google font

--- parallax-effect/src/App.jsx
+++ parallax-effect/src/App.jsx
@@ -1,5 +1,7 @@
-
+import greenLogo from '/BC tech academy mockup.png'
+import orangeLogo from '/BC_tech_academy_logo-no_bg.png'
 import './App.css'
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
 
