b81979a9c01a2230c929a329bb5706e0d9751dcb
BCEESAY10 <bamfaceesay30@gmail.com>
2025-08-10 03:47:59 +0000

    remove errors

--- parallax-effect/src/App.jsx
+++ parallax-effect/src/App.jsx
@@ -1,12 +1,60 @@
-import greenLogo from '/BC tech academy mockup.png'
-import orangeLogo from '/BC_tech_academy_logo-no_bg.png'
+import greenLogo from './assets/BC tech academy mockup.png'
+import orangeLogo from './assets/BC_tech_academy_logo-no_bg.png'
 import './App.css'
+import { useRef } from 'react'
 import { Parallax, ParallaxLayer } from '@react-spring/parallax'
 
 function App() {
+    const ref = useRef();
 
   return (
-    <h1>Welcome</h1>
+    <main>
+      <Parallax pages={4} ref={ref}>
+
+        <ParallaxLayer
+          offset={0}
+          speed={1}
+          factor={2}
+          style={{
+            backgroundImage: `url(${greenLogo})`,
+            backgroundSize: 'cover',
+          }}
+        />
+
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          factor={4}
+          style={{
+            backgroundImage: `url(${orangeLogo})`,
+            backgroundSize: 'cover',
+          }}
+        ></ParallaxLayer>
+
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: 'center' }}
+        >
+          <img src={greenLogo} />
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={0.2}
+          speed={0.05}
+          onClick={() => ref.current.scrollTo(3)}
+        >
+          <h1>Welcome to my website</h1>
+        </ParallaxLayer>
+
+        <ParallaxLayer
+          offset={3}
+          speed={2}
+          onClick={() => ref.current.scrollTo(0)}
+        >
+          <h2>Welcome Y'all!</h2>
+        </ParallaxLayer>
+      </Parallax>
+    </main>
   )
 }
 
